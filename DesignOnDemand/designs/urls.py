9ac811918e6de2a68d79525b62f4e14ce0dc9d60
TheVandiProject <vandiel.gonzalez@upr.edu>
2023-10-08 12:39:18 -0400

    Implemented Log in and Sign up authentication

--- DesignOnDemand/designs/urls.py
+++ DesignOnDemand/designs/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# app_name = "designs"
+
 urlpatterns = [
     path("", views.main_page, name="main_page"),
     path('login/', views.login_view, name='login'),
