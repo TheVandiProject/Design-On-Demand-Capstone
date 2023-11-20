cbe660514b41bd2636e0b3f7242b6e0cdebf22c4
TheVandiProject <vandiel.gonzalez@upr.edu>
2023-11-20 18:19:52 -0400

    Uncommented the admin pages

--- DesignOnDemand/settings.py
+++ DesignOnDemand/settings.py
@@ -50,7 +50,7 @@ else:
 
 INSTALLED_APPS = [
     'designs.apps.DesignsConfig',
-    #'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
