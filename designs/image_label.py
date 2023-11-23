79d5f86caf383d305169becdc2aafdc5470e14e4
TheVandiProject <vandiel.gonzalez@upr.edu>
2023-11-23 00:21:31 -0400

    Added Sample Images and started to attempt results

--- designs/image_label.py
+++ designs/image_label.py
@@ -10,110 +10,184 @@ def load_labels(filename):
     return [line.strip() for line in f.readlines()]
 
 def classify_image(request):
-    """Takes an uploaded image as a query and returns the predictions for two models."""
+    """Takes an uploaded image as a query and returns the predictions."""
     image_file = request.FILES["image"]
     
-    # Load the first model
-    interpreter1 = tf.lite.Interpreter(model_path="designs\models\DoD-Model.tflite")
-    interpreter1.allocate_tensors()
+    #Load model
+    interpreter = tf.lite.Interpreter(model_path="designs\models\Diverse_Model-DoD.tflite")
+    # interpreter = tf.lite.Interpreter(model_path="designs\models\Second-DoD-Model.tflite")
+    interpreter.allocate_tensors()
     
-    # Get the input and output details of the first model
-    input_details1 = interpreter1.get_input_details()
-    output_details1 = interpreter1.get_output_details()
+     # Get the input and output details of the model.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
     
-    # Load the second model
-    interpreter2 = tf.lite.Interpreter(model_path="designs\models\Second-DoD-Model.tflite")
-    interpreter2.allocate_tensors()
+    floating_model = input_details[0]['dtype'] == np.float32
+   
+    #Prepare the input data
+    height = input_details[0]['shape'][1]
+    width = input_details[0]['shape'][2]
+    img = Image.open(image_file).resize((width, height))
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+    # score = tf.nn.softmax(predictions[0])
     
-    # Get the input and output details of the second model
-    input_details2 = interpreter2.get_input_details()
-    output_details2 = interpreter2.get_output_details()
+    # img = np.frombuffer(img.tobytes(), dtype=np.uint8)
     
-    # Prepare the input data for the first model
-    height1, width1 = input_details1[0]['shape'][1], input_details1[0]['shape'][2]
-    img1 = Image.open(image_file).resize((width1, height1))
-    img_array1 = tf.keras.utils.img_to_array(img1)
-    img_array1 = tf.expand_dims(img_array1, 0)  # Create a batch for the first model
+    # input_data = np.expand_dims(img, axis=0)
     
-    # Set the input tensor of the first model
-    interpreter1.set_tensor(input_details1[0]['index'], img_array1)
-    interpreter1.invoke()
-    
-    # Get the output data of the first model
-    output_data1 = interpreter1.get_tensor(output_details1[0]['index'])
-    results1 = np.squeeze(output_data1)
-    
-    # Process results for the first model
-    softmax_output1 = tf.nn.softmax(results1)
-    score1 = softmax_output1.numpy()
-    top_n1 = np.argsort(score1)[-5:][::-1]
-    
-    labels1 = load_labels("designs\models\labels.txt")
-    
-    predictions1 = []
-    for i in top_n1:
-        prediction1 = {
-            "label": labels1[i].replace("_", " ").title(),
-            "confidence": "{:.2f}%".format(score1[i] * 100),
-        }
-        predictions1.append(prediction1)
-    
-    # Prepare the input data for the second model
-    height2, width2 = input_details2[0]['shape'][1], input_details2[0]['shape'][2]
-    img_array2 = tf.image.resize(img_array1, [height2, width2])  # Resize for the second model
-    
-    # Set the input tensor of the second model
-    interpreter2.set_tensor(input_details2[0]['index'], img_array2)
-    interpreter2.invoke()
-    
-    # Get the output data of the second model
-    output_data2 = interpreter2.get_tensor(output_details2[0]['index'])
-    results2 = np.squeeze(output_data2)
-    
-    # Process results for the second model
-    softmax_output2 = tf.nn.softmax(results2)
-    score2 = softmax_output2.numpy()
-    top_n2 = np.argsort(score2)[-5:][::-1]
-    
-    labels2 = load_labels("designs\models\labels.txt")  # Replace with the correct labels file
-    
-    predictions2 = []
-    for i in top_n2:
-        prediction2 = {
-            "label": labels2[i].replace("_", " ").title(),
-            "confidence": "{:.2f}%".format(score2[i] * 100),
-        }
-        predictions2.append(prediction2)
-    
-    # Merge results and keep the highest confidence for shared labels
-    merged_predictions = {}
-    for prediction in predictions1 + predictions2:
-        label = prediction["label"]
-        confidence = float(prediction["confidence"][:-1])  # Remove '%' and convert to float
+    if floating_model:
+        input_data = (np.float32(img) - 127.5) / 127.5
         
-        if label in merged_predictions:
-            # Update with the highest confidence
-            merged_predictions[label] = max(merged_predictions[label], confidence)
+    #Set the inpout tensor of the model
+    interpreter.set_tensor(input_details[0]['index'], img_array)
+    interpreter.invoke()
+    
+    #Get output data of the model
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    
+    #Squeeze to remove extra dimensions
+    results = np.squeeze(output_data)
+    
+    softmax_output = tf.nn.softmax(results)
+    
+    score = softmax_output.numpy()
+    
+    top_n = np.argsort(score)[-5:][::-1]
+    
+    # labels = load_labels("designs\models\labels.txt")
+    labels = load_labels("designs\models\detailed-labels.txt")
+    
+    top_predictions = labels[top_n[0]].replace("_", " ").title()
+    top_score = "{:.2f}%".format(score[top_n[0]] * 100)
+    
+    predictions = []
+    for i in top_n[1:]:
+        if 0 <= i < len(labels):
+            prediction={
+                "label": labels[i].replace("_", " ").title(),
+                "confidence": "{:.2f}%" .format(score[i]*100),
+            }
+            predictions.append(prediction)     
         else:
-            merged_predictions[label] = confidence
-    
-    # Sort by confidence in descending order
-    sorted_merged_predictions = [
-        {"label": label, "confidence": f"{confidence:.2f}%", "score": confidence} 
-        for label, confidence in sorted(merged_predictions.items(), key=lambda x: x[1], reverse=True)
-    ]
-    
-    # Separate the top prediction and other predictions
-    top_prediction = sorted_merged_predictions[0]
-    other_predictions = sorted_merged_predictions[1:5]
-    
-    # Take the top 5 results
+            prediction={
+                # "label": f"Invalid index: {i}",
+                "label": "Other",
+                "confidence": "{:.2f}%" .format(score[i]*100),
+            }
+            predictions.append(prediction)
+
     final_result = {
-        "top_predictions": top_prediction["label"],
-        "top_score": top_prediction["confidence"],
-        "other_predictions": other_predictions,
+    "top_predictions": top_predictions,
+    "top_score": top_score,
+    "other_predictions": predictions,
     }
+    return final_result    
+    # """Takes an uploaded image as a query and returns the predictions for two models."""
+    # image_file = request.FILES["image"]
     
-    return final_result
+    # # Load the first model
+    # interpreter1 = tf.lite.Interpreter(model_path="designs\models\DoD-Model.tflite")
+    # interpreter1.allocate_tensors()
+    
+    # # Get the input and output details of the first model
+    # input_details1 = interpreter1.get_input_details()
+    # output_details1 = interpreter1.get_output_details()
+    
+    # # Load the second model
+    # interpreter2 = tf.lite.Interpreter(model_path="designs\models\Second-DoD-Model.tflite")
+    # interpreter2.allocate_tensors()
+    
+    # # Get the input and output details of the second model
+    # input_details2 = interpreter2.get_input_details()
+    # output_details2 = interpreter2.get_output_details()
+    
+    # # Prepare the input data for the first model
+    # height1, width1 = input_details1[0]['shape'][1], input_details1[0]['shape'][2]
+    # img1 = Image.open(image_file).resize((width1, height1))
+    # img_array1 = tf.keras.utils.img_to_array(img1)
+    # img_array1 = tf.expand_dims(img_array1, 0)  # Create a batch for the first model
+    
+    # # Set the input tensor of the first model
+    # interpreter1.set_tensor(input_details1[0]['index'], img_array1)
+    # interpreter1.invoke()
+    
+    # # Get the output data of the first model
+    # output_data1 = interpreter1.get_tensor(output_details1[0]['index'])
+    # results1 = np.squeeze(output_data1)
+    
+    # # Process results for the first model
+    # softmax_output1 = tf.nn.softmax(results1)
+    # score1 = softmax_output1.numpy()
+    # top_n1 = np.argsort(score1)[-5:][::-1]
+    
+    # labels1 = load_labels("designs\models\labels.txt")
+    
+    # predictions1 = []
+    # for i in top_n1:
+    #     prediction1 = {
+    #         "label": labels1[i].replace("_", " ").title(),
+    #         "confidence": "{:.2f}%".format(score1[i] * 100),
+    #     }
+    #     predictions1.append(prediction1)
+    
+    # # Prepare the input data for the second model
+    # height2, width2 = input_details2[0]['shape'][1], input_details2[0]['shape'][2]
+    # img_array2 = tf.image.resize(img_array1, [height2, width2])  # Resize for the second model
+    
+    # # Set the input tensor of the second model
+    # interpreter2.set_tensor(input_details2[0]['index'], img_array2)
+    # interpreter2.invoke()
+    
+    # # Get the output data of the second model
+    # output_data2 = interpreter2.get_tensor(output_details2[0]['index'])
+    # results2 = np.squeeze(output_data2)
+    
+    # # Process results for the second model
+    # softmax_output2 = tf.nn.softmax(results2)
+    # score2 = softmax_output2.numpy()
+    # top_n2 = np.argsort(score2)[-5:][::-1]
+    
+    # labels2 = load_labels("designs\models\labels.txt")  # Replace with the correct labels file
+    
+    # predictions2 = []
+    # for i in top_n2:
+    #     prediction2 = {
+    #         "label": labels2[i].replace("_", " ").title(),
+    #         "confidence": "{:.2f}%".format(score2[i] * 100),
+    #     }
+    #     predictions2.append(prediction2)
+    
+    # # Merge results and keep the highest confidence for shared labels
+    # merged_predictions = {}
+    # for prediction in predictions1 + predictions2:
+    #     label = prediction["label"]
+    #     confidence = float(prediction["confidence"][:-1])  # Remove '%' and convert to float
+        
+    #     if label in merged_predictions:
+    #         # Update with the highest confidence
+    #         merged_predictions[label] = max(merged_predictions[label], confidence)
+    #     else:
+    #         merged_predictions[label] = confidence
+    
+    # # Sort by confidence in descending order
+    # sorted_merged_predictions = [
+    #     {"label": label, "confidence": f"{confidence:.2f}%", "score": confidence} 
+    #     for label, confidence in sorted(merged_predictions.items(), key=lambda x: x[1], reverse=True)
+    # ]
+    
+    # # Separate the top prediction and other predictions
+    # top_prediction = sorted_merged_predictions[0]
+    # other_predictions = sorted_merged_predictions[1:5]
+    
+    # # Take the top 5 results
+    # final_result = {
+    #     "top_predictions": top_prediction["label"],
+    #     "top_score": top_prediction["confidence"],
+    #     "other_predictions": other_predictions,
+    # }
+    
+    # return final_result
 
     
--- designs/views.py
+++ designs/views.py
@@ -1,15 +1,17 @@
 # from fileinput import filename
 # from django.core.files.base import ContentFile
+import os
+import random
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from .forms import *
+from .forms import RegisterForm, UploadDesignForm
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponse
 # from .models import UploadDesign
 from designs.forms import UploadDesignForm
 # from django.core.files.storage import FileSystemStorage
-# from django.conf import settings
+from django.conf import settings
 from PIL import Image # Pillow library for image resizing
 from .image_label import classify_image
 # from django.db import models
@@ -19,14 +21,17 @@ from .models import ImageUpload
 def main_page_view(request):
     return render(request, 'designs/main_page.html')
 
+
 def index_view(request):
     return render(request, 'designs/index.html')
+
 
 def render_home_view(request):
     if(request.user.is_authenticated):
         return render(request, 'designs/user_home.html')
     else:
         return render(request, 'designs/NonUser_Home.html')
+
 
 def login_view(request):
     if request.method == "POST":
@@ -48,6 +53,7 @@ def login_view(request):
     form = AuthenticationForm()    
     return render(request, 'designs/login_page.html', {'login_form': form})
 
+
 def signup_view(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
@@ -63,10 +69,50 @@ def signup_view(request):
       
     return render(request, 'designs/sign_up.html', {'signin_form': form})
 
+
 def logout_view(request):
     logout(request)
     messages.info(request, "You have successfully logged out.", extra_tags='success')
     return redirect("/") 
+
+
+def get_random_images(label, num_images=10):
+    try:
+        base_path = os.path.join(settings.STATIC_ROOT, 'designs', 'images')
+        if not os.path.exists(base_path):
+            return f"The directory {base_path} does not exist."
+
+        images = [os.path.join('designs', 'images', label, img)
+                 for img in os.listdir(base_path)
+                 if os.path.isfile(os.path.join(base_path, img))]
+
+        if not images:
+            return "The directory is empty."
+
+        return random.sample(images, min(num_images, len(images)))
+    except Exception as e:
+        return str(e)
+
+
+def swiper_view(request):
+    if request.user.is_authenticated:
+        template_name = 'designs/user_home.html'
+    else:
+        template_name = 'designs/NonUser_Home.html'
+    # Call the classify_image function to get predictions
+    classification_result = classify_image(request)
+    
+    top_prediction_images = get_random_images(classification_result["top_predictions"])
+    
+    other_prediction_images = {}
+    for prediction in classification_result["other_predictions"]:
+        label = prediction["label"]
+        images = get_random_images(label)
+        other_prediction_images[label] = images
+
+    return render(request, template_name, {'top_prediction_images': top_prediction_images, 'other_prediction_images': other_prediction_images})
+
+
 
 def upload_design_view(request):
     form = UploadDesignForm()
