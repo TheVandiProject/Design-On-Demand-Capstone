570ef3717adc7e4597217ad0cd1d475a6962cc5d
TheVandiProject <vandiel.gonzalez@upr.edu>
2023-11-26 23:22:29 -0400

    changed urls

--- DesignOnDemand/urls.py
+++ DesignOnDemand/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     # path("", views.index, name="index"),
     # path('', include("designs.urls")),
-    path('/', include('designs.urls')),
+    path('', include('designs.urls')),
     path('admin/', admin.site.urls),
 ]  # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
